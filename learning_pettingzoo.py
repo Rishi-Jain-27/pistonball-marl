eb0b3a2d09342ac653f69b27ae4b147945728250
Rishi Jain <27rishijainpersonalemail@gmail.com>
2026-06-10 12:54:31 -0400

    Updated learningpettiingzoo

--- learning_pettingzoo.py
+++ learning_pettingzoo.py
@@ -8,33 +8,6 @@ from pettingzoo.butterfly import pistonball_v6
 import pettingzoo
 
 if __name__ == '__main__':
-    env = pistonball_v6.env(n_pistons=20, time_penalty=-0.1, continuous=True,
-    random_drop=True, random_rotate=True, ball_mass=0.75, ball_friction=0.3,
-    ball_elasticity=1.5, max_cycles=125, render_mode='human')
-    env.reset() # resets env and sets it up for use when called the first time
-
-    # for each agent — AEC
-    for agent in env.agent_iter():
-        observation, reward, termination, truncation, info = env.last()
-
-        if termination or truncation:
-            action = None
-        else:
-            # invalid action masking
-            if isinstance(observation, dict) and "action_mask" in observation:
-                mask = observation["action_mask"]
-            else:
-                mask = None
-
-            # this is where you would insert your policy
-            action = env.action_space(agent).sample(mask=mask)
-
-        # takes and executes the action of the agent in the environment
-        # automatically switches control to next agent
-        env.step(action)
-    env.close()
-    
-
     """
     AEC API usage
     env = pistonball_v6.env(n_pistons=20, time_penalty=-0.1, continuous=True,
@@ -64,7 +37,7 @@ ball_elasticity=1.5, max_cycles=125, render_mode='human')
 
     """
     Parallel API usage
-    env = pistonball_v6.parallel_env(n_pistons=20, time_penalty=-0.1, continuous=True,
+    parallel_env = pistonball_v6.parallel_env(n_pistons=20, time_penalty=-0.1, continuous=True,
 random_drop=True, random_rotate=True, ball_mass=0.75, ball_friction=0.3,
 ball_elasticity=1.5, max_cycles=125, render_mode='human')
     observations, infos = parallel_env.reset(seed=42)
